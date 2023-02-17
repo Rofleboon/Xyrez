2c1b13ce49a756a2befbf18c4630a9eab380b8d3
rofleboon <rofleboon@gmail.com>
2023-02-17 16:36:25 +0100

    added callback

--- login.php
+++ login.php
@@ -16,12 +16,12 @@ session_start();
 
 if(isset($_POST['login'])){
     // Redirect the user to the Battle.net login page
-    $client_id = 'your_client_id';
-    $redirect_uri = 'https://example.com/callback.php';
+    $client_id = '17d284d6c3594fafa8085ee0eafca4cf';
+    $redirect_uri = 'https://www.xyrez.de/callback.php';
     $scope = 'wow.profile';
     $state = 'some_random_string';
 
-    $login_url = 'https://us.battle.net/oauth/authorize';
+    $login_url = 'https://oauth.battle.net/authorize';
     $login_url .= '?response_type=code';
     $login_url .= '&client_id='.urlencode($client_id);
     $login_url .= '&redirect_uri='.urlencode($redirect_uri);
