479644a2692c8699096dc01b5879f9dd8bb33825
rofleboon <rofleboon@gmail.com>
2023-02-20 15:00:19 +0100

    fetchtest update

--- js/fetchtest.js
+++ js/fetchtest.js
@@ -1,35 +1,50 @@
-function fetchLogs() {
-    const apikey = "4a17488a491b8980ee6c89d6c1ae8b94";
-    const url = `https://www.warcraftlogs.com/v1/reports/guild/ist%20Gildenlos/MalGanis/EU?api_key=${apikey}`;
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error: Could not fetch data.");
+function fetchdata() {
+    fetch('https://www.warcraftlogs.com:443/v1/reports/guild/ist%20Gildenlos/MalGanis/EU?api_key=4a17488a491b8980ee6c89d6c1ae8b94')
+      .then(res => {
+        if (!res.ok) {
+          throw Error("API is down");
         }
-        return response.json();
+        return res.json()
       })
-      .then((data) => {
-        const logsContainer = document.querySelector("#logs");
-        logsContainer.innerHTML = data
-          .slice(0, 15)
-          .map((log) => {
-            const logDate = new Date(log.start);
-            const locale = "de-DE";
-            const options = { weekday: "long", year: "numeric", month: "2-digit", day: "2-digit" };
-            const formattedDate = logDate.toLocaleDateString(locale, options);
-            return `
-              <div class="container entry">
-                <p class="logname">${formattedDate} ${log.title}</p>
-                <a href="https://www.warcraftlogs.com/reports/${log.id}" target="_blank" class="btn btn-primary">Warcraftlogs</a>
-                <a href="https://www.wipefest.gg/report/${log.id}" target="_blank" class="btn btn-primary">Wipefest</a>
-              </div>`;
-          })
-          .join("");
+      .then(data => {
+        const html = data.slice(0, 15).map(wcl =>{              
+          return `                
+            <div class="container entry ">
+              <p class="logname"2>${timeConverter(wcl.start)} ${wcl.title}</p>
+              <a href =\"https://www.warcraftlogs.com/reports/${wcl.id}\" target = _blank class = "btn btn-primary">Warcraftlogs</a>               
+              <a href =\"https://www.wipefest.gg//report/${wcl.id}\" target = _blank class="btn btn-primary">Wipefest</a><br>                        
+            </div>                  
+          `                
+        }).join('')            
+        document
+          .querySelector('#logs')
+          .insertAdjacentHTML('afterbegin', html);
       })
-      .catch((error) => {
-        console.error(error);
+      .catch(error => {
+        // display error message to the user
+        document.querySelector('#logs').innerHTML = "<p>Failed to load logs. Please try again later.</p>";
+        console.log(error);
       });
   }
   
-  fetchLogs();
+  function getDayName(dateStr, locale) {
+    var date = new Date(dateStr);
+    return date.toLocaleDateString(locale, { weekday: 'long' });        
+  }
+  
+  function timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp);
+    var months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var day = getDayName(a, "de-DE");
+    var time = day + ', ' + date + '.' + month + '.' + year;
+    return time;
+  }
+  
+  fetchdata();
   
